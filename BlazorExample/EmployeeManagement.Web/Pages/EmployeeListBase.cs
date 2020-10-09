428fdea4f16ff651574c2b99fb4616f2286c1bdb
Ivan <ivanbeljaev@mail.ru>
2020-10-09 20:41:05 +0500

    Added RestAPI

--- BlazorExample/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ BlazorExample/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -29,7 +29,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "David@pragimtech.com",
                 DateOfBrith = new DateTime(1980, 10, 5),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/john.png"
             };
 
@@ -41,7 +41,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "Sam@pragimtech.com",
                 DateOfBrith = new DateTime(1981, 12, 22),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                DepartmentId = 2,
                 PhotoPath = "images/sam.jpg"
             };
 
@@ -53,7 +53,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "mary@pragimtech.com",
                 DateOfBrith = new DateTime(1979, 11, 11),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 3,
                 PhotoPath = "images/mary.png"
             };
 
@@ -65,7 +65,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "sara@pragimtech.com",
                 DateOfBrith = new DateTime(1982, 9, 23),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 4,
                 PhotoPath = "images/sara.png"
             };
 
