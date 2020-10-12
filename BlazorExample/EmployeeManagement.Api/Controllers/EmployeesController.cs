aad50ebc0891be68542b0234115844aa5d9d785f
Ivan Belyaev <Ivan.Belyaev@emerson.com>
2020-10-12 07:24:25 +0500

    added DeleteEmployee

--- BlazorExample/EmployeeManagement.Api/Controllers/EmployeesController.cs
+++ BlazorExample/EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -71,5 +71,28 @@ namespace EmployeeManagement.Api.Controllers
                     "Error creating new employee record");
             }
         }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            try
+            {
+                var employeeToDelete = await employeeRepository.GetEmployee(id);
+
+                if (employeeToDelete == null)
+                {
+                    return NotFound($"Employee with Id = {id} not found");
+                }
+
+                return await employeeRepository.DeleteEmployee(id);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error deleting data");
+            }
+        }
     }
+}
 }
