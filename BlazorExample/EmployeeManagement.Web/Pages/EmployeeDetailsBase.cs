ebd149764ffd77145d7469d4b8ae18c52450cb37
Ivan Belyaev <Ivan.Belyaev@emerson.com>
2020-10-14 13:49:51 +0500

    Added show/hide info

--- BlazorExample/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ BlazorExample/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages
 {
@@ -23,6 +24,21 @@ namespace EmployeeManagement.Web.Pages
         public IEmployeeService EmployeeService { get; set; }
 
         /// <summary>
+        /// Gets, sets Coordinates
+        /// </summary>
+        protected string Coordinates { get; set; }
+
+        /// <summary>
+        /// Button Text
+        /// </summary>
+        protected string ButtonText { get; set; } = "Hide Footer";
+
+        /// <summary>
+        /// Css Class
+        /// </summary>
+        protected string CssClass { get; set; } = null;
+
+        /// <summary>
         /// Id
         /// (Id) matches with the name of the route parameter in the @page directive (@page "/employeedetails/{Id}")
         /// </summary>
@@ -33,6 +49,23 @@ namespace EmployeeManagement.Web.Pages
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        /// <summary>
+        /// Button Click handler
+        /// </summary>
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
     }
 }
